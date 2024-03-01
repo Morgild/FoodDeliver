78c5d1f4490fb4013ff4cb58d53d955925a4cc24
Munkh-Orgil Lkhavgajav <23LP0045@G301-CC0188.local>
2024-03-01 10:37:54 +0800

    deploy connect

--- front/src/app/common/axios/index.ts
+++ front/src/app/common/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://fooddelivery-khjj.onrender.com/",
   headers: { "Content-Type": "application/json" },
 });
