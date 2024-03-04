e15f9199102e56c753a4818af9611d2b38ce61b8
Morgild <you@example.com>
2024-03-04 16:28:17 +0800

    deploy backend3

--- front/src/app/common/axios/index.ts
+++ front/src/app/common/axios/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:3001/",
   baseURL: "https://fooddelivery1-6xyr.onrender.com/",
   headers: { "Content-Type": "application/json" },
 });
