efc2049e91faa66839a7686cc1467dc91032f8e2
Morgild <you@example.com>
2024-06-14 13:25:28 +0800

    dashboard container1

--- front/src/app/Dashboard/page.tsx
+++ front/src/app/Dashboard/page.tsx
@@ -2,14 +2,13 @@ import { Container } from "@mui/material";
 
 export default function Dashboard() {
   return (
-    <Container>
+    <>
       <iframe
         title="Monartex"
-        width="1140"
-        height="541.25"
+        width="100%"
         src="https://app.powerbi.com/reportEmbed?reportId=5e4bbb3b-8392-46a6-8d22-e8f17b751a92&autoAuth=true&ctid=abde62c4-46fa-4bd7-9585-391b26488815"
         allowFullScreen={true}
       ></iframe>
-    </Container>
+    </>
   );
 }
