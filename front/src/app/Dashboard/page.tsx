05953e9164c3c9fa7db32814f7984e5dfea3f082
Morgild <you@example.com>
2024-06-14 13:04:20 +0800

    dashboard

---
+++ front/src/app/Dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <>
+      <iframe
+        title="Monartex"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=5e4bbb3b-8392-46a6-8d22-e8f17b751a92&autoAuth=true&ctid=abde62c4-46fa-4bd7-9585-391b26488815"
+        allowFullScreen={true}
+      ></iframe>
+    </>
+  );
+}
